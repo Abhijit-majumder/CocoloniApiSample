3dcd149ec1e36d32cf8a831390ff57b84fdab30d
Abhijit Majumder <ts-abhiji.a.majumder@rakuten.com>
2020-09-17 11:42:43 +0900

    Cocoloni api call on buttton click

---
+++ app/src/main/java/com/example/cocoloniapitest/FirstFragment.kt
@@ -0,0 +1,74 @@
+package com.example.cocoloniapitest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.fragment_first.*
+import org.json.JSONObject
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            callApi()
+        }
+    }
+
+    private fun callApi() {
+        val requestQueue = Volley.newRequestQueue(this.context)
+        val url = "https://api-lovefortune.zappallas.com/logicsrv2/api/v1.0/service/menus/LF_0400"
+        val jsonObjectRequest = CustomJsonObjectRequestBasicAuth(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                textview_first.text = "Response : $response"
+            },
+            Response.ErrorListener { error ->
+                // TODO: Handle error
+            }
+        )
+        requestQueue.add(jsonObjectRequest)
+    }
+
+    // Class to make a volley json object request with basic authentication
+    class CustomJsonObjectRequestBasicAuth(
+        method:Int, url: String,
+        jsonObject: JSONObject?,
+        listener: Response.Listener<JSONObject>,
+        errorListener: Response.ErrorListener,
+    )
+        : JsonObjectRequest(method,url, jsonObject, listener, errorListener) {
+
+
+        @Throws(AuthFailureError::class)
+        override fun getHeaders(): Map<String, String> {
+            val headers = HashMap<String, String>()
+            headers["Content-Type"] = "application/json"
+            //val credentials:String = "username:password"
+            val auth = "Basic UmFrdXRlbkxpbms6RTJiZEh4OE4="
+            headers["Authorization"] = auth
+            return headers
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cocoloniapitest/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.cocoloniapitest
+
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_first.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
